d2587603dfd6553f2aefb3504e6a9435cfc9354a
tyannk <41520010057@student.mercubuana.ac.id>
2022-09-27 15:20:04 +0700

    Pertemuan 4

--- Latihan/myproject/application/controllers/Blog.php
+++ Latihan/myproject/application/controllers/Blog.php
@@ -1,16 +1,12 @@
 <?php
-Class Blog extends CI_Controller
-{
-    public function index(){
-        echo "Selamat Datang";
-    }
-    public function listdata()
-	{
-		$this->load->view('list_data');
+class Blog extends CI_Controller{
+	function __construct() {
+		parent::__construct();
 	}
-    public function detail()
-	{
-		$this->load->view('detail_blog');
+
+	function index(){
+		$data['title'] = "This Is Title";
+		$data['content'] = "This Is The Contents";
+		$this->load->view('blog_view',$data);
 	}
 }
-?>

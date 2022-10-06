728bf16fd8337770a87a69ea5ea0089c4220d65a
tyannk <tyannur77@gmail.com>
2022-10-06 10:07:54 +0700

    Merapihkan tampilan kode

--- Latihan/myproject/application/views/percobaan.php
+++ Latihan/myproject/application/views/percobaan.php
@@ -2,6 +2,10 @@
     <h1>Halaman Percobaan!</h1>
 
     <div id="body">
-        <p align="center"><font size=6 color="blue">Ini dibuat untuk percobaan</p>
-        <p><marquee bgcolor="yellow">Ini dibuat untuk percobaan.</p>
+        <p align="center">
+            <font size=6 color="blue">Ini dibuat untuk percobaan
+        </p>
+        <p>
+            <marquee bgcolor="yellow">Ini dibuat untuk percobaan.
+        </p>
     </div>

1af72d88a02a5f439d46267973349dd6c2ec310d
tyannk <tyannur77@gmail.com>
2022-10-24 21:43:25 +0700

    Menambah fitur login, register dan menambah database user

---
+++ Latihan/myproject/application/controllers/Home.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('auth');
+        $this->auth->cek_login();
+    }
+
+    public function index()
+    {
+        $this->load->view('vw_home');
+    }
+}
